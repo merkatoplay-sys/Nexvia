d7c66f82e3579e25d2b47fcd6c58d67d85574956
merkatoplay-sys <merkatoplay@gmail.com>
2025-12-28 16:41:52 -0600

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,19 +1,17 @@
-import express, { type Express } from "express";
-import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+import type { Express } from "express";
+import express from "express";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
-  if (!fs.existsSync(distPath)) {
-    throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
-    );
-  }
+  const publicPath = path.resolve(__dirname, "public");
 
-  app.use(express.static(distPath));
+  app.use(express.static(publicPath));
 
-  // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"));
   });
 }
